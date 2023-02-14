3862395cdf475fbc91483cd862723acbc697f673
Fellipe <fellipe.lauton@praticabr.com>
2023-02-14 17:12:16 -0300

    feat: ensure SignUpController return 400 if no email is provide

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,8 @@
 export class SignUpController {
   handle(httpRequest: any): any {
-    return { statusCode: 400, body: new Error("No name was provided") };
+    if (!httpRequest.body.name)
+      return { statusCode: 400, body: new Error("No name was provided") };
+    if (!httpRequest.body.email)
+      return { statusCode: 400, body: new Error("No email was provided") };
   }
 }

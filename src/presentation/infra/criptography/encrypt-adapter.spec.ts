17374d56a0d19e3491023c4472c4bf6aa4f66a0f
Fellipe Lauton <lipelauton@gmail.com>
2023-02-15 15:25:00 -0300

    test: ensure BcryptAdapter returns a hash on success

--- src/presentation/infra/criptography/encrypt-adapter.spec.ts
+++ src/presentation/infra/criptography/encrypt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import { BcryptAdapter } from "./encrypt-adapter";
 import bcrypt from "bcrypt";
 
+jest.mock("bcrypt", () => ({
+  async hash(): Promise<string> {
+    return new Promise((resolve) => resolve("hash"));
+  },
+}));
+
 const makeSut = (salt: number): BcryptAdapter => {
   return new BcryptAdapter(salt);
 };
@@ -12,5 +18,12 @@ describe("Bcrypt Adapter", () => {
     jest.spyOn(bcrypt, "hash");
     await sut.encrypt("any_value");
     expect(bcrypt.hash).toHaveBeenCalledWith("any_value", salt);
+  });
+
+  test("should return a hash on success", async () => {
+    const salt = 12;
+    const sut = makeSut(salt);
+    const hash = await sut.encrypt("any_value");
+    expect(hash).toBe("hash");
   });
 });

f568b4ea4af41a02c7caf6a0bff3d509a047059a
Fellipe Lauton <lipelauton@gmail.com>
2023-02-27 14:11:02 -0300

    test: ensure LoginController returns 500 if Authenticate throws

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -74,6 +74,17 @@ describe("Login Controller", () => {
     expect(httpResponse).toEqual(serverError(new Error()));
   });
 
+  test("should return 500 if Authenticate throws", async () => {
+    const { sut, authenticationStub } = makeSut();
+    jest
+      .spyOn(authenticationStub, "auth")
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      );
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(serverError(new Error()));
+  });
+
   test("should call email validator with correct email", async () => {
     const { sut } = makeSut();
     const httpRequest = {

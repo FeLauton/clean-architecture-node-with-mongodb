cb7b669d371671a99bd3463c8012e06d5cefbb04
Fellipe Lauton <lipelauton@gmail.com>
2023-03-07 08:51:47 -0300

    test: ensure LoginRoutes/login return 200 on login

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,6 +1,10 @@
+import { hash } from "bcrypt";
+import { Collection } from "mongodb";
 import request from "supertest";
 import { MongoHelper } from "../../infra/db/mongodb/helpers/mongo-helpers";
 import app from "../config/app";
+
+let accountCollection: Collection;
 
 describe("Login Routes", () => {
   beforeAll(async () => {
@@ -8,7 +12,7 @@ describe("Login Routes", () => {
   });
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection("accounts");
+    accountCollection = await MongoHelper.getCollection("accounts");
     await accountCollection.deleteMany({});
   });
 
@@ -25,6 +29,23 @@ describe("Login Routes", () => {
           email: "fellipe.lauton@gmail.com",
           password: "123",
           passwordConfirmation: "123",
+        })
+        .expect(200);
+    });
+  });
+  describe("POST /login", () => {
+    test("Should return 200 on login", async () => {
+      const hashedPassword = await hash("123", 12);
+      await accountCollection.insertOne({
+        name: "Fellipe",
+        email: "fellipe.lauton@gmail.com",
+        password: hashedPassword,
+      });
+      await request(app)
+        .post("/api/login")
+        .send({
+          email: "fellipe.lauton@gmail.com",
+          password: "123",
         })
         .expect(200);
     });

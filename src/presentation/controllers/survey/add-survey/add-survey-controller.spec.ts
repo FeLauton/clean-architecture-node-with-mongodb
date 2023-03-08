30a7b7338dd24b0d6957d4c8dd60b9b49b7f4932
Fellipe <fellipe.lauton@praticabr.com>
2023-03-08 15:09:11 -0300

    test: ensure AddSurvey return 204 AddSurvey on success

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -1,5 +1,9 @@
 import { AddSurveyController } from "./add-survey-controller";
-import { badRequest, serverError } from "../../../helpers/http/http-helpers";
+import {
+  badRequest,
+  noContent,
+  serverError,
+} from "../../../helpers/http/http-helpers";
 import {
   HttpRequest,
   AddSurvey,
@@ -87,5 +91,11 @@ describe("Add Survey Controller", () => {
       );
     const httpResponse = await sut.handle(makeFakeRequest());
     expect(httpResponse).toEqual(serverError(new Error()));
+  });
+
+  test("Should return 204 on success", async () => {
+    const { sut } = makeSut();
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(noContent());
   });
 });

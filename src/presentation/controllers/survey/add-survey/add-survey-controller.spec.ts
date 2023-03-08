8309af4e359c96e8ebc2f6ae5616dc66cd1963de
Fellipe <fellipe.lauton@praticabr.com>
2023-03-08 15:06:25 -0300

    test: ensure AddSurvey returns 500 if throws

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -1,5 +1,5 @@
 import { AddSurveyController } from "./add-survey-controller";
-import { badRequest } from "../../../helpers/http/http-helpers";
+import { badRequest, serverError } from "../../../helpers/http/http-helpers";
 import {
   HttpRequest,
   AddSurvey,
@@ -76,5 +76,16 @@ describe("Add Survey Controller", () => {
     const httpRequest = makeFakeRequest();
     await sut.handle(httpRequest);
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body);
+  });
+
+  test("Should return 500 if AddSurvey throws", async () => {
+    const { sut, addSurveyStub } = makeSut();
+    jest
+      .spyOn(addSurveyStub, "add")
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      );
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(serverError(new Error()));
   });
 });

a2eb147e7859faca8fbd5f8eba8c46bf906a856e
Fellipe Lauton <lipelauton@gmail.com>
2023-03-08 12:09:46 -0300

    ensure AddSurveyController calls Validation with correct values

---
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -0,0 +1,19 @@
+import { serverError } from "../../../helpers/http/http-helpers";
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+  Validation,
+} from "../../../protocols";
+
+export class AddSurveyController implements Controller {
+  constructor(private readonly validation: Validation) {}
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      this.validation.validate(httpRequest.body);
+      return null;
+    } catch (error) {
+      return serverError(error);
+    }
+  }
+}

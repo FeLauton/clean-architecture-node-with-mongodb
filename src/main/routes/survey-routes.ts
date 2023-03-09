9cb9360edff1387b3753dcb3ace7d82675f8bcf1
Fellipe Lauton <lipelauton@gmail.com>
2023-03-09 09:49:17 -0300

    refactor: rename route survey to surveys

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -3,5 +3,5 @@ import { adaptRoute } from "../adapters/express/express-route-adapter";
 import { makeSurveyController } from "../factories/controllers/add-survey/add-survey-controler-factory";
 
 export default (router: Router): void => {
-  router.post("/survey", adaptRoute(makeSurveyController()));
+  router.post("/surveys", adaptRoute(makeSurveyController()));
 };

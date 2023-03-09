6c9f37d2c8f8ddebb6820943f924c40d864a0c56
Fellipe <fellipe.lauton@praticabr.com>
2023-03-09 12:35:12 -0300

    test: ensure AuthMiddleware return 403 if LoadAccountByToken returns null

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -11,6 +11,10 @@ const makeFakeAccount = (): AccountModel => ({
   password: "valid_password",
 });
 
+const makeFakeRequest = () => ({
+  headers: { "x-access-token": "any_token" },
+});
+
 const makeLoadAccountByTokenStub = (): LoadAccountByToken => {
   class LoadAccountByTokenStub implements LoadAccountByToken {
     async load(
@@ -47,9 +51,16 @@ describe("Auth Middleware", () => {
   test("should call LoadAccountByToken with correct accessToken", async () => {
     const { sut, loadAccountByTokenStub } = makeSut();
     const loadSpy = jest.spyOn(loadAccountByTokenStub, "load");
-    await sut.handle({
-      headers: { "x-access-token": "any_token" },
-    });
+    await sut.handle(makeFakeRequest());
     expect(loadSpy).toHaveBeenCalledWith("any_token");
+  });
+
+  test("should return 403 if LoadAccountByToken returns null", async () => {
+    const { sut, loadAccountByTokenStub } = makeSut();
+    jest
+      .spyOn(loadAccountByTokenStub, "load")
+      .mockReturnValueOnce(new Promise((resolve) => resolve(null)));
+    const httpResponse = await sut.handle({});
+    expect(httpResponse).toEqual(forbidden(new AccessDeniedError()));
   });
 });

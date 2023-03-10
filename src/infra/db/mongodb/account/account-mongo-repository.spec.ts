f82d9a35aaa5ad6d9d7bcb417d25434991a71c07
Fellipe <fellipe.lauton@praticabr.com>
2023-03-10 15:37:59 -0300

    test: ensure AccountMongoRepository return an account on loadAccountByToken success without role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -74,4 +74,21 @@ describe("Account Mongo Repository", () => {
       expect(account?.accessToken).toBe("any_token");
     });
   });
+  describe("loadAccountByToken()", () => {
+    test("Should return an account on loadAccountByToken success without role", async () => {
+      const sut = makeSut();
+      await accountCollection.insertOne({
+        name: "any_name",
+        email: "any_email@mail.com",
+        password: "any_password",
+        accessToken: "any_token",
+      });
+      const account = await sut.loadAccountByToken("any_token");
+      expect(account).toBeTruthy();
+      expect(account.id).toBeTruthy();
+      expect(account.name).toBe("any_name");
+      expect(account.email).toBe("any_email@mail.com");
+      expect(account.password).toBe("any_password");
+    });
+  });
 });

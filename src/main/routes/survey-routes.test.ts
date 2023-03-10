d1f315cc92446b18b2bdd81f0479302a13181e24
Fellipe <fellipe.lauton@praticabr.com>
2023-03-10 16:05:09 -0300

    test: ensure AddSurveyRoute  return 403 on add survey without accessToken

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -20,7 +20,7 @@ describe("Survey Routes", () => {
   });
 
   describe("POST /survey", () => {
-    test("Should return 204 on survey", async () => {
+    test("Should return 403 on add survey without accessToken", async () => {
       await request(app)
         .post("/api/surveys")
         .send({
@@ -35,7 +35,7 @@ describe("Survey Routes", () => {
             },
           ],
         })
-        .expect(204);
+        .expect(403);
     });
   });
 });

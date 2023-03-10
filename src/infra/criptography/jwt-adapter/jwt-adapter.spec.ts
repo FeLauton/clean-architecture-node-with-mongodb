3a751b3bcad3ad850f94d5098dc25d2648c9689c
Fellipe <fellipe.lauton@praticabr.com>
2023-03-10 13:35:42 -0300

    test: ensure JwtAdapter calls verify with correct values

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,9 +1,12 @@
 import { JwtAdapter } from "./jwt-adapter";
-import jwt from "jsonwebtoken";
+import jwt, { verify } from "jsonwebtoken";
 
 jest.mock("jsonwebtoken", () => ({
   async sign(): Promise<string> {
     return new Promise((resolve) => resolve("any_token"));
+  },
+  async verify(): Promise<string> {
+    return new Promise((resolve) => resolve("any_value"));
   },
 }));
 
@@ -19,6 +22,7 @@ describe("Jwt Adapter", () => {
       await sut.encrypt("any_id");
       expect(signSpy).toHaveBeenCalledWith({ id: "any_id" }, "secret");
     });
+
     test("Should return a token on sign success", async () => {
       const sut = makeSut();
       const accessToken = await sut.encrypt("any_id");
@@ -33,5 +37,29 @@ describe("Jwt Adapter", () => {
       const promise = sut.encrypt("any_value");
       await expect(promise).rejects.toThrow();
     });
+  });
+
+  describe("verify()", () => {
+    test("Should calls verify with correct values", async () => {
+      const sut = makeSut();
+      const verifySpy = jest.spyOn(jwt, "verify");
+      await sut.decrypt("any_token");
+      expect(verifySpy).toHaveBeenCalledWith("any_token", "secret");
+    });
+
+    // test("Should return a token on decrypt success", async () => {
+    //   const sut = makeSut();
+    //   const accessToken = await sut.decrypt("any_token");
+    //   expect(accessToken).toBe("any_id");
+    // });
+
+    // test("Should throw if decrypt throws", async () => {
+    //   const sut = makeSut();
+    //   jest.spyOn(jwt, "verify").mockImplementationOnce(() => {
+    //     throw new Error();
+    //   });
+    //   const promise = sut.decrypt("any_value");
+    //   await expect(promise).rejects.toThrow();
+    // });
   });
 });

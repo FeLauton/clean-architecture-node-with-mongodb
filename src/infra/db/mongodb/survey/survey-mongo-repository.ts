fbb7a686de482fb2f7cfece9dcfbab4ce94e7ea0
Fellipe <fellipe.lauton@praticabr.com>
2023-03-13 11:47:35 -0300

    feat: ensure serveyMongoRepository load all surveys on success

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,10 +1,20 @@
+import { LoadSurveyRepository } from "./../../../../data/protocols/db/survey/load-survey-repository";
 import { AddSurveyRepository } from "../../../../data/protocols/db/survey/add-survey-repository";
 import { AddSurveyModel } from "../../../../domain/usecases/add-survey";
 import { MongoHelper } from "../helpers/mongo-helpers";
+import { SurveyModel } from "../../../../domain/models/survey";
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository
+  implements AddSurveyRepository, LoadSurveyRepository
+{
   async add(surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = MongoHelper.getCollection("surveys");
     await surveyCollection.insertOne(surveyData);
+  }
+
+  async loadAll(): Promise<SurveyModel[]> {
+    const surveyCollection = MongoHelper.getCollection("surveys");
+    const surveys = await surveyCollection.find().toArray();
+    return surveys.map((survey) => MongoHelper.mongoIdMap(survey));
   }
 }

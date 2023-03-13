6589615d9ae8eb0e91e236b727925163af74e731
Fellipe <fellipe.lauton@praticabr.com>
2023-03-13 12:45:20 -0300

    refactor: move auth and authAdmin to a factory function

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,13 +1,13 @@
+import { auth } from "./../middlewares/auth";
 import { adaptMiddleware } from "./../adapters/express-middleware-adapter";
 import { makeAuthMiddleware } from "./../factories/middlewares/auth-middleware-factory";
 import { Router } from "express";
 import { adaptRoute } from "../adapters/express-route-adapter";
 import { makeLoadSurveysController } from "../factories/controllers/survey/load-surveys/load-surveys-controller-factory";
 import { makeAddSurveyController } from "../factories/controllers/survey/add-survey/add-survey-controler-factory";
+import { adminAuth } from "../middlewares/admin-auth";
 
 export default (router: Router): void => {
-  const adminAuth = adaptMiddleware(makeAuthMiddleware("admin"));
-  const userAuth = adaptMiddleware(makeAuthMiddleware("user"));
   router.post("/surveys", adminAuth, adaptRoute(makeAddSurveyController()));
-  router.get("/surveys", userAuth, adaptRoute(makeLoadSurveysController()));
+  router.get("/surveys", auth, adaptRoute(makeLoadSurveysController()));
 };

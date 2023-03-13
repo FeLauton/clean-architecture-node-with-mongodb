a183a7039d4872a72bf81b4c1b0ed1cbaa7f7a54
Fellipe <fellipe.lauton@praticabr.com>
2023-03-13 07:57:19 -0300

    chore: ajustes setup Jest

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,7 @@ module.exports = {
     "<rootDir>/src/**/*.ts",
     "!<rootDir>/src/main/**",
     "!<rootDir>/src/**/*protocols.ts",
+    "!<rootDir>/src/presentation/protocols/index.ts",
   ],
   coverageDirectory: "coverage",
   coverageProvider: "babel",

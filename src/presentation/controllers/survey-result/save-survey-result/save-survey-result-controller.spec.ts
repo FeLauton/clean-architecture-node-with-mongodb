1f235f6b381452afee7563b837897e5851d751be
Fellipe <fellipe.lauton@praticabr.com>
2023-03-14 16:12:11 -0300

    test: ensure SaveSurveyResultController return 500 if LoadSurveyById throws

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
@@ -1,3 +1,6 @@
+import { ServerError } from "./../../../errors/server-error";
+import { InvalidParamError } from "./../../../errors/invalid-param-error";
+import { forbidden, serverError } from "./../../../helpers/http/http-helpers";
 import { SaveSurveyResultController } from "./save-survey-result-controller";
 import {
   HttpRequest,
@@ -88,9 +91,28 @@ describe("SaveSurveyResultController", () => {
   test("Should calls LoadSurveyById with correct values", async () => {
     const { sut, loadSurveyByIdStub } = makeSut();
     const loadByIdSpy = jest.spyOn(loadSurveyByIdStub, "loadById");
-    const httpRequest = makeFakeRequest();
-    await sut.handle(httpRequest);
+    await sut.handle(makeFakeRequest());
     expect(loadByIdSpy).toHaveBeenCalledWith("any_survey_id");
+  });
+
+  test("Should return 403 if LoadSurveyById returns null", async () => {
+    const { sut, loadSurveyByIdStub } = makeSut();
+    jest
+      .spyOn(loadSurveyByIdStub, "loadById")
+      .mockReturnValueOnce(new Promise((resolve) => resolve(null)));
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(forbidden(new InvalidParamError("surveyId")));
+  });
+
+  test("Should return 500 if LoadSurveyById throws", async () => {
+    const { sut, loadSurveyByIdStub } = makeSut();
+    jest
+      .spyOn(loadSurveyByIdStub, "loadById")
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      );
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(serverError(new Error()));
   });
 
   test("Should calls SaveSurveyResult with correct values", async () => {

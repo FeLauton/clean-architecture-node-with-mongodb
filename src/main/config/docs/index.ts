0a28265147a51cea228397246c1ad688ab93766e
Fellipe <fellipe.lauton@praticabr.com>
2023-03-24 12:45:08 -0300

    feat: add docs to swagger

--- src/main/config/docs/index.ts
+++ src/main/config/docs/index.ts
@@ -1,8 +1,37 @@
+import components from "./components";
+import paths from "./paths";
+import schemas from "./schemas";
+
 export default {
   openapi: "3.0.0",
   info: {
-    title: "Clean Node Api",
-    description: "Clean node Api documentation",
+    title: "4Dev - Enquetes para Programadores",
+    description:
+      "Essa é a documentação da API feita pelo instrutor Rodrigo Manguinho no curso da Udemy de NodeJs usando Typescript, TDD, Clean Architecture e seguindo os princípios do SOLID e Design Patterns.",
     version: "1.0.0",
+    contact: {
+      name: "Fellipe Lauton",
+      email: "lipe_lauton@gmail.com",
+      url: "https://www.linkedin.com/in/fellipe-lauton/",
+    },
   },
+  servers: [
+    {
+      url: "/api",
+      description: "Servidor Principal",
+    },
+  ],
+  tags: [
+    {
+      name: "Login",
+      description: "APIs relacionadas a Login",
+    },
+    {
+      name: "Enquete",
+      description: "APIs relacionadas a Enquete",
+    },
+  ],
+  paths,
+  schemas,
+  components,
 };

84a09a2de9d8d8100f06faa682d82334a0413be1
Fellipe <fellipe.lauton@praticabr.com>
2023-03-24 16:26:08 -0300

    test: esnure DbLoadSurveyResult calls LoadSurveyResultRepository with correct value

---
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
@@ -0,0 +1,44 @@
+import { mockLoadSurveyResultRepository } from "./../../../tests/mock-db-survey-result";
+import { DbLoadSurveyResult } from "./db-load-survey-result";
+import { LoadSurveyResultRepository } from "./db-load-survey-result-protocols";
+
+interface SutTypes {
+  sut: DbLoadSurveyResult;
+  loadSurveyResultRepositoryStub: LoadSurveyResultRepository;
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyResultRepositoryStub = mockLoadSurveyResultRepository();
+  const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub);
+  return {
+    sut,
+    loadSurveyResultRepositoryStub,
+  };
+};
+
+describe("DbLoadSurveyResult", () => {
+  test("Should call LoadSurveyResultRepository with correct values", async () => {
+    const { sut, loadSurveyResultRepositoryStub } = makeSut();
+    const loadBySurveyIdSpy = jest.spyOn(
+      loadSurveyResultRepositoryStub,
+      "loadBySurveyId"
+    );
+    await sut.load("any_survey_id");
+    expect(loadBySurveyIdSpy).toHaveBeenCalledWith("any_survey_id");
+  });
+
+  // test("Should throw if LoadSurveyResultRepository throws", async () => {
+  //   const { sut, loadSurveyResultRepositoryStub } = makeSut();
+  //   jest
+  //     .spyOn(loadSurveyResultRepositoryStub, "load")
+  //     .mockImplementationOnce(throwError);
+  //   const promise = sut.load(mockLoadSurveyResultParams());
+  //   expect(promise).rejects.toThrow();
+  // });
+
+  // test("Should return survey result on success", async () => {
+  //   const { sut } = makeSut();
+  //   const surveyResult = await sut.load(mockLoadSurveyResultParams());
+  //   expect(surveyResult).toEqual(mockSurveyResultModel());
+  // });
+});
